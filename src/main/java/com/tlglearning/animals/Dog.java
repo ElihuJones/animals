9131dd8359b14e9e5828fd2d9f78a04f8dd0518b
ElihuJones <xelijone@amazon.com>
2022-09-20 13:07:12 -0400

    Initialization Demonstration.
    Truly get this down. It appears to be fundamental to
    programming understanding. LEARN IT!

--- src/main/java/com/tlglearning/animals/Dog.java
+++ src/main/java/com/tlglearning/animals/Dog.java
@@ -2,6 +2,45 @@ package com.tlglearning.animals;
 
 public class Dog extends Wolf {
 
+  private static final int MY_CONSTANT = initializeValue(); 
+
+  static {
+    System.out.println("Dog initialization block");
+  }
+
+  //Declare and initialize a field. This is wrong location for example to compiler
+  private int data = initializaData();
+
+  //Instance initialization block
+  {
+    System.out.println("Dog instance initialization block");
+  }
+
+  {
+    System.out.println("Second Dog initialization block");
+  }
+
+  //Constructor
+  public Dog() {
+    System.out.println("Dog constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Dog.initializeValue");
+    return -2;
+  }
+
+  //Methods
+  public static void whoAmI() {
+    System.out.println("I am a dog!");
+  }
+
+  //Method. Normally behind constructor.
+  private int initializaData() {
+    System.out.println("Dog.initializeData");
+    return 2;
+  }
+
   //Override of inherited Wolf class
   @Override
   public void vocalize() {
@@ -11,11 +50,6 @@ public class Dog extends Wolf {
   @Override
   public void hunt() {
     System.out.println("Hunt for balls, bones, leaves, etc.");
-  }
-
-  //Methods
-  public static void whoAmI() {
-    System.out.println("I am a dog!");
   }
 
   public void rollOver() {
--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -2,9 +2,42 @@ package com.tlglearning.animals;
 
 public class Wolf {
 
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int data = initializeData(); //Declaration with assignment is a field
+
+  //Static initialization code that will be executed when the class
+  static {
+    System.out.println("Wolf static initialization block");
+  }
+
+  //This is an instance initialization block
+  {
+    System.out.println("Wolf instance initialization block");
+  }
+
+  //Constructor. Every constructor has exactly 1 super() whether explicit or implicit in bytecode by the compiler.
+  //So if super() is empty you do not need to add it.
+  public Wolf() {
+    System.out.println("Wolf constructor");
+  }
+
+
+  private static int initializeValue() {
+    System.out.println("Wolf.intializeValue");
+    return -1;
+  }
+
+  private int initializeData() {
+    System.out.println("Wolf.initializeData");
+    return -1;
+  }
+
   public void vocalize() {
+
     System.out.println("Howl like a wolf!");
   }
+
 
   public void hunt() {
     System.out.println("Hunt in packs for live prey");
